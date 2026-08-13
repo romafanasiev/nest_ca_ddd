320b6357adaab1707896c7cd83bfaaed32a29ae3
romafanasiev <romafanasiev123@gmail.com>
2026-08-13 11:42:33 +0300

    feat: add product port

---
+++ src/product/application/ports/product.repository.port.ts
@@ -0,0 +1,16 @@
+import { Product } from 'src/product/domain/entitites/product.entity';
+import { ProductId } from 'src/product/domain/value-objects/product-id.vo';
+
+export const PRODUCT_REPOSITORY = Symbol('PRODUCT_REPOSITORY');
+
+export interface ProductFilters {
+  isActive?: boolean;
+  minPrice?: number;
+  maxPrice?: number;
+}
+
+export interface ProductRepository {
+  save(product: Product): Promise<void>;
+  findById(id: ProductId): Promise<Product | null>;
+  findAll(filters: ProductFilters): Promise<Product[]>;
+}
