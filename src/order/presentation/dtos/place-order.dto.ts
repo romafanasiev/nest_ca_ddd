1d68f24343afd05f829eeb7db054c461670a831a
romafanasiev <romafanasiev123@gmail.com>
2026-08-25 10:07:05 +0300

    feat: place order dto

---
+++ src/order/presentation/dtos/place-order.dto.ts
@@ -0,0 +1,63 @@
+import { Type } from 'class-transformer';
+import {
+  ArrayMinSize,
+  IsArray,
+  IsNumber,
+  IsOptional,
+  IsString,
+  IsUUID,
+  MaxLength,
+  Min,
+  MinLength,
+  ValidateNested,
+} from 'class-validator';
+
+export class PlaceOrderItemDto {
+  @IsUUID()
+  productID: string;
+
+  @IsString()
+  @MinLength(1)
+  @MaxLength(255)
+  productName: string;
+
+  @IsNumber()
+  @Min(0)
+  unitPrice: number;
+
+  @IsString()
+  @MinLength(3)
+  @MaxLength(3)
+  @IsOptional()
+  currency?: string = 'USD';
+
+  @IsNumber()
+  @Min(1)
+  quantity: number;
+}
+
+export class PlaceOrderDto {
+  @IsUUID()
+  customerId: string;
+
+  @IsArray()
+  @ValidateNested({ each: true })
+  @ArrayMinSize(0)
+  @Type(() => PlaceOrderItemDto)
+  items: PlaceOrderItemDto[];
+
+  @IsString()
+  shippingStreet: string;
+
+  @IsString()
+  shippingCity: string;
+
+  @IsString()
+  shippingState: string;
+
+  @IsString()
+  shippingZipCode: string;
+
+  @IsString()
+  shippingCounty: string;
+}
